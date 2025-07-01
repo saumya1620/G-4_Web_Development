6a8dd6b2bb36479313df80a6ff7e027ac9a4f611
Saumya Jain <122440288+saumya1620@users.noreply.github.com>
2025-07-01 15:50:38 +0530

    Create script.js

---
+++ Lecture1_Introduction/script.js
@@ -0,0 +1,90 @@
+// console.log("HELLO WORLD")
+// var a = 123;
+// console.log(a);
+// function abc ()
+// {
+//     console.log("this is function abc");
+//     function hgf()
+//     {
+//         console.log("THIS IS FUNCTION HGF");
+//     }
+//     hgf();
+// }
+// function def()
+// {
+//     console.log("THIS IS FUNCTION DEF");
+// }
+// abc();
+
+// var xyz = () =>
+// {
+//     console.log("XYZ IS ARROW FUNCTION");
+// }
+// xyz();
+// var b = 300;
+// console.log(b);
+
+
+// {
+//     //compound statement
+//     let a=100;
+//     let b=200;
+//     let c=300;
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+//     console.log(x);
+//     {
+//         console.log(a);
+//         console.log(b);
+//         console.log(c);
+//         let d=200;
+//         {
+//             var x = 2000;
+//             console.log(a);
+//             console.log(b);
+//             console.log(c);
+//             console.log(d);
+//         }
+//     }
+// }
+//var d;
+// function abc()
+// {
+//     var a=100;
+//     var b=200;
+//     var c = 300;
+//     console.log(d);
+//     function def()
+//     {
+//         var d=400;
+//         function ijk()
+//         {
+//             console.log(a);
+//             console.log(b);
+//             console.log(c);
+//             console.log(d);
+//         }
+//         ijk();
+//     }
+//     def();
+     
+// }
+// abc();
+
+//if var is used then the final value of i saved globally will be printed if let then local 
+// for(var i=0;i<=5;i++)
+// {
+//     function closure(i)
+//     {
+//  setTimeout(()=>
+// {
+//     console.log(i);
+// },2000)  
+
+// }
+// closure(i)}
+
+//closure is a combination of function bundled together wiith ref to its surrounding state
+//advantage = data hiding,encapsulation 
+//disadvantage closure hold memory and space, consuption of memory , not garbage collected 
