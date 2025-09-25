b7f8055fa6fe10840ad4832f63aa44729ecca843
Saumya1620 <saumyajain598@gmail.com>
2025-09-25 09:25:21 +0530

    lecture 20 and 22

---
+++ Lecture22/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement(
+    "div",
+    {id:"parent", key:1},
+    [
+    React.createElement(
+        "div",
+        {id:"child1",key:4},
+        React.createElement("h1",{id:"h1hv" , key:2}, "this is child1 heading")
+     ),
+    React.createElement(
+        "div",
+        {id:"child2",key:5},
+        React.createElement("h1",{id:"h2dh"},"this is child")
+
+    )]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading)
